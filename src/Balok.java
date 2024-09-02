b6a277d91bb1fe851e4a4ff12608738d60207869
Jonathan0823 <eguinjonathan08@gmail.com>
2024-09-02 10:41:25 +0700

    fix comments

--- src/Balok.java
+++ src/Balok.java
@@ -6,14 +6,13 @@ public class Balok extends Kubus {
     int lebar;
     int tinggi;
 
-    // Melakukan polimorfisme dari class Kubus
     public Balok(int panjang, int lebar, int tinggi) {
         super(panjang);
         this.lebar = lebar;
         this.tinggi = tinggi;
     }
 
-    // Method menghitung volume dan luas permukaan
+    // override method volume dan luasPermukaan
     public void volume() {
         int volume = panjang * lebar * tinggi;
         System.out.println("Volume Balok = " + volume);
